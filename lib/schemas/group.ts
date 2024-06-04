71223d78e442c5f35b87eff1bf4ee9cff3ca7f9e
Saikiran Patil <patilrsaikiran@gmail.com>
2024-06-05 01:02:49 +0530

    fix: allow group creation without avatar

--- lib/schemas/group.ts
+++ lib/schemas/group.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const CreateGroupSchema = z.object({
     name: z.string().min(3, "Name should be atleast 3 characters"),
     image: z.string().optional().refine((value) => {
-        if (!value) return;
+        if (!value) return true;
         const base64Size = (value.length * (3 / 4)) - (value.endsWith('==') ? 2 : (value.endsWith('=') ? 1 : 0));
         return base64Size <= 2000 * 1024;
     }, "File size should be less than 2MB"),
